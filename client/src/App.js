ce04c9d06646286894343d6ab6c9cbfcd36923ec
KJ Schelling <kjschelling@KJs-MBP-2.lan>
2023-10-10 22:07:31 -0700

    added navbar component, added button to zip code form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import AirIndexCard from "./components/AirIndexCard";
 import ZipCodeForm from "./components/ZipCodeForm";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      {/* <AirIndexCard /> */}
-      <ZipCodeForm />
+      <NavBar />
+      <div class="container">
+        <div className="d-inline-flex">
+          {/* <AirIndexCard /> */}
+          <ZipCodeForm />
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/ZipCodeForm.jsx
+++ client/src/components/ZipCodeForm.jsx
@@ -2,14 +2,20 @@ import React from "react";
 
 export default function ZipCodeForm() {
   return (
-    <div>
-      <h1 class>Air Quality Index Information </h1>
+    <div className="">
+      <h1 class="mt-5">Air Quality Index Information </h1>
       <div class="form-outline">
-        <div class="card p-5">
+        <div class="card p-5 w-80 mt-5">
           <label class="form-label" for="form12">
             Enter your zip code
           </label>
-          <input type="text" id="form12" class="form-control" />
+          <input
+            type="text"
+            id="form12"
+            placeholder="Enter your zipcode"
+            class="form-control"
+          />
+          <button class="btn btn-primary m-4">Get Info</button>
         </div>
       </div>
     </div>
