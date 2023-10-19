f7451fcb24b771cc536c6dddc2eafbeaf9c95d48
KJ Schelling <kjschelling@KJs-MBP-2.lan>
2023-10-19 12:55:34 -0700

    moved state into form component, form printing to the console on submit

--- client/src/App.js
+++ client/src/App.js
@@ -18,38 +18,13 @@ import axios from "axios";
 //gets the data from the input fetches data from the geocode API to send to Air pollution API
 
 function App() {
-
-  const [zipCode, setZipCode] = useState();
-  const [longitude, setLongitude] = useState();
-  const [latitude, setLatitude] = useState();
-  
-
-//write a function to get long & lat data with geocode
-
-  async function getLongitudeLatitude(e) {
-    e.preventDefault();
-    try {
-      await axios.post("http://localhost:3002/post_zipCode", {
-        zipCode,
-      });
-    } catch (error) {
-      console.log("Error", error);
-    }
-  }
-
-
-
-  
-
-  // write a function to handle submit
-
   return (
     <div className="App">
       <NavBar />
       <div className="container">
         <div className="d-inline-flex">
           {/* <AirIndexCard /> based on a conditional */}
-          <ZipCodeForm setZipCode={setZipCode} postZipCode={postZipCode} />
+          <ZipCodeForm />
         </div>
       </div>
     </div>
--- client/src/components/ZipCodeForm.jsx
+++ client/src/components/ZipCodeForm.jsx
@@ -1,30 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function ZipCodeForm({ zipCode, setZipCode, postZipCode }) {
-  const handleSubmit = () => {
-    postZipCode(zipCode);
+class ZipCodeForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      zipcode: "",
+    };
+  }
+
+  handleZipCodeChange = (e) => {
+    this.setState({ zipcode: e.target.value });
   };
-  return (
-    <div className="">
-      <h1 className="mt-5">Air Quality Index Information </h1>
-      <div className="form-outline">
-        <form onSubmit={handleSubmit} className="card p-5 w-80 mt-5">
-          <label className="form-label" htmlFor="zipCode">
-            Enter your zip code
-          </label>
-          <input
-            type="text"
-            name="zipCode"
-            id="zipCode"
-            onChange={(e) => setZipCode(e.target.value)}
-            placeholder="Enter zipcode"
-            className="form-control"
-          />
-          <button className="btn btn-primary m-4" type="submit">
-            Get Info
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+
+  //handle submit makes an API call to geocode API to get long / lat from zipcode
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.zipcode);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1 className="mt-5">Air Quality Index Information </h1>
+        <div className="form-outline">
+          <div className="card p-5 w-80 mt-5">
+            <label className="form-label" htmlFor="zipCode">
+              Zip Code
+            </label>
+            <input
+              type="text"
+              name="zipcode"
+              value={this.state.zipcode}
+              onChange={this.handleZipCodeChange}
+              placeholder="Enter ZIP Code"
+              className="form-control"
+            />
+            <button className="btn btn-primary m-4" type="submit">
+              Get Info
+            </button>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
+
+export default ZipCodeForm;
